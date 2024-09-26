22faf44aefbe9433bb6e7439bebd9ba9630e3848
ZerataX <mail@zera.tax>
2024-09-26 00:20:19 +0000

    bukkitPlugins.bluemap-offline-player-markers: init at 3.0

--- pkgs/bukkit-plugins/default.nix
+++ pkgs/bukkit-plugins/default.nix
@@ -6,6 +6,7 @@ lib.makeScope pkgs.newScope (self:
     with self; {
       bluemap = callPackage ./bluemap {};
       bluemap-marker-manager = callPackage ./bluemap-marker-manager {};
+      bluemap-offline-player-markers = callPackage ./bluemap-offline-player-markers {};
       harbor = callPackage ./harbor {};
       dynmap = callPackage ./dynmap {};
       discordsrv = callPackage ./discordsrv {};
