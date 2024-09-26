b064724432f2764185a0539aea8b7dca3dff47b5
ZerataX <mail@zera.tax>
2024-09-26 00:18:28 +0000

    bukkitPlugins.bluemap: init at 5.4

--- pkgs/bukkit-plugins/default.nix
+++ pkgs/bukkit-plugins/default.nix
@@ -1,9 +1,12 @@
-{ lib, pkgs }:
-
+{
+  lib,
+  pkgs,
+}:
 lib.makeScope pkgs.newScope (self:
-  with self; {
-    harbor = callPackage ./harbor { };
-    dynmap = callPackage ./dynmap { };
-    discordsrv = callPackage ./discordsrv { };
-    simple-voice-chat = callPackage ./simple-voice-chat { };
-  })
+    with self; {
+      bluemap = callPackage ./bluemap {};
+      harbor = callPackage ./harbor {};
+      dynmap = callPackage ./dynmap {};
+      discordsrv = callPackage ./discordsrv {};
+      simple-voice-chat = callPackage ./simple-voice-chat {};
+    })
