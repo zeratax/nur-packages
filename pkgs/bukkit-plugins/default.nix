933c468034f5401403996f191732a1eb0bd0343e
ZerataX <mail@zera.tax>
2024-09-26 00:23:08 +0000

    formatting

--- pkgs/bukkit-plugins/default.nix
+++ pkgs/bukkit-plugins/default.nix
@@ -7,9 +7,9 @@ lib.makeScope pkgs.newScope (self:
       bluemap = callPackage ./bluemap {};
       bluemap-marker-manager = callPackage ./bluemap-marker-manager {};
       bluemap-offline-player-markers = callPackage ./bluemap-offline-player-markers {};
-      paper-tweaks = callPackage ./paper-tweaks {};
-      harbor = callPackage ./harbor {};
-      dynmap = callPackage ./dynmap {};
       discordsrv = callPackage ./discordsrv {};
+      dynmap = callPackage ./dynmap {};
+      harbor = callPackage ./harbor {};
+      paper-tweaks = callPackage ./paper-tweaks {};
       simple-voice-chat = callPackage ./simple-voice-chat {};
     })
